47e620665fb0d86fb76d3ceb06328ae1959da4d0
PowerfulPaw <golovskoy.nickolay@gmail.com>
2016-04-29 18:03:20 +0300

    fewChanges

--- index.js
+++ index.js
@@ -45,39 +45,39 @@ $("#text").click(function () {
 
     }, 0);
 });
-$("#buttonExit").click(function(){
+$("#buttonExit").click(function () {
     $("#yourName").hide();
     $("#buttonExit").hide();
     token = undefined;
 
 });
-$("#usernameRegistration").click(function() {
+$("#usernameRegistration").click(function () {
     setTimeout(function () {
         $("#usernameRegistration").popover('hide');
     }, 0)
 });
-$("#passwordRegistration").click(function(){
-    setTimeout(function(){
-    $("#passwordRegistration").popover('hide');
-},0);
+$("#passwordRegistration").click(function () {
+    setTimeout(function () {
+        $("#passwordRegistration").popover('hide');
+    }, 0);
 });
-$("#usernameAuthorisation").click(function() {
+$("#usernameAuthorisation").click(function () {
     setTimeout(function () {
         $("#usernameAuthorisation").popover('hide');
     }, 0)
 });
-$("#passwordAuthorisation").click(function(){
-    setTimeout(function(){
+$("#passwordAuthorisation").click(function () {
+    setTimeout(function () {
         $("#passwordAuthorisation").popover('hide');
-    },0);
+    }, 0);
 });
 
 function doSignUp() {
-        username = $("#usernameRegistration")[0];
-    var password = $("#passwordRegistration")[0];
+        username = $("#usernameRegistration");
+    var password = $("#passwordRegistration");
     var obj = {
-        username: username.value,
-        password: password.value
+        username: username[0].value,
+        password: password[0].value
     };
     var txt;
     var jsn = JSON.stringify(obj);
@@ -87,32 +87,35 @@ function doSignUp() {
     xhr.send(jsn);
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4) {
-            if (username.value == "" || password.value == "") {
-                var username1 = $("#usernameRegistration");
-                var password1 = $("#passwordRegistration");
-                username1.attr({
+            if (username[0].value == "") {
+                username.attr({
                     'data-toggle': 'popover',
                     'data-placement': 'bottom',
                     'data-content': "Shouldn't be empty"
                 });
-                username1.popover('show');
-                password1.attr({
-                    'data-toggle': 'popover',
-                    'data-placement': 'bottom',
-                    'data-content': "Shouldn't be empty"
-                });
-                password1.popover('show');
-                setTimeout(function(){
-                    username1.popover('hide');
-                    password1.popover('hide');
-                },2000)
+                username.popover('show');
+                setTimeout(function () {
+                    username.popover('hide');
+                }, 2000)
             }
+            if (password[0].value == "") {
+                password.attr({
+                    'data-toggle': 'popover',
+                    'data-placement': 'bottom',
+                    'data-content': "Shouldn't be empty"
+                });
+                password.popover('show');
+                setTimeout(function () {
+                    password.popover('hide');
+                }, 2000)
+            }
+
         }
-        if (username.value !== "" || password.value !== ""){
+        if (username[0].value !== "" && password[0].value !== "") {
             if (xhr.readyState == 4) {
                 if (xhr.status == 201) {
                     $("#prodList").find("p").remove();
-                    $("<p/>", {text: "You enter as " + username.value, id: "yourName"}).appendTo("#prodList");
+                    $("<p/>", {text: "You enter as " + username[0].value, id: "yourName"}).appendTo("#prodList");
                     $("#buttonExit").show();
                     $("#signUp").slideToggle("slow");
                     txt = JSON.parse(xhr.responseText);
@@ -135,12 +138,11 @@ function doSignUp() {
 }
 
 function doSignIn() {
-
-    username = $("#usernameAuthorisation")[0];
-    var password = $("#passwordAuthorisation")[0];
+    username = $("#usernameAuthorisation");
+    var password = $("#passwordAuthorisation");
     var obj = {
-        username: username.value,
-        password: password.value
+        username: username[0].value,
+        password: password[0].value
     };
     var jsn = JSON.stringify(obj);
     var xhr = new XMLHttpRequest();
@@ -149,37 +151,38 @@ function doSignIn() {
     xhr.send(jsn);
     xhr.onreadystatechange = function () {
         if (xhr.readyState == 4) {
-            if (username.value == "" || password.value == "") {
-                var username1 = $("#usernameAuthorisation");
-                var password1 = $("#passwordAuthorisation");
-               username1.attr({
+            if (username[0].value == "") {
+                username.attr({
                     'data-toggle': 'popover',
                     'data-placement': 'bottom',
                     'data-content': "Shouldn't be empty"
                 });
-               username1.popover('show');
-               password1.attr({
-                    'data-toggle': 'popover',
-                    'data-placement': 'bottom',
-                    'data-content': "Shouldn't be empty"
-                });
-               password1.popover('show');
+                username.popover('show');
                 setTimeout(function () {
-                    username1.popover('hide');
-                    password1.popover('hide');
+                    username.popover('hide');
+                }, 2000)
+            }
+            if (password[0].value == "") {
+                password.attr({
+                    'data-toggle': 'popover',
+                    'data-placement': 'bottom',
+                    'data-content': "Shouldn't be empty"
+                });
+                password.popover('show');
+                setTimeout(function () {
+                    password.popover('hide');
                 }, 2000)
             }
         }
-        if (username.value !== "" || password.value !== "") {
+        if (username[0].value !== "" && password[0].value !== "") {
             if (xhr.readyState == 4) {
                 if (xhr.status == 200) {
                     var a = JSON.parse(xhr.responseText);
                     if (a.success == true) {
                         $("#prodList").find("p").remove();
-                        $("<p/>", {text: "You enter as " + username.value, id: "yourName"}).appendTo("#prodList");
+                        $("<p/>", {text: "You enter as " + username[0].value, id: "yourName"}).appendTo("#prodList");
                         $("#buttonExit").show();
                         $("#signIn").toggle('slow');
-
                         token = a.token; //take token into the global scope for sending comments
                     }
                 }
@@ -189,7 +192,6 @@ function doSignIn() {
                     setTimeout(function () {
                         $("#signInFailure").toggle('slow');
                     }, 2500);
-
                 }
             }
         }
@@ -218,9 +220,6 @@ function showProduct(value) {
                 cont.innerHTML = '<div id="' + id + '" class="goods">' + '<h3>' + title +
                     '</h3>' + '<img " src="http://smktesting.herokuapp.com/static/' + img + '">' +
                     '<article>' + 'Product description:' + '<br>' + text1 + '</article>' + '</div>';
-
-
-                //Comments
                 val = val + 1;
                 var xhr1 = new XMLHttpRequest();
                 xhr1.open('GET', 'http://smktesting.herokuapp.com/api/reviews/' + val + '');
@@ -240,9 +239,7 @@ function showProduct(value) {
                                 '<p>Rate: ' + rate + '</p>' +
                                 '<p>' + text + '</p>' + '</div>';
                         }
-
                     }
-
                 };
                 xhr1.send();
             }
@@ -253,25 +250,19 @@ function showProduct(value) {
 
 function getStarValue(value) {
     starsValue = +value;
-
-
 }
 
 function sendComment() {
-    var textArea = $("#text");
-    var text = textArea[0];
+    var text = $("#text");
     var obj = {
         rate: starsValue,
-        text: text.value
+        text: text[0].value
     };
     var jsn = JSON.stringify(obj);
     var xhr2 = new XMLHttpRequest();
     var wantToSignUp = $("#wantToSignUp");
     var wantToSignIn = $("#wantToSignIn");
-
     var stars = $("#reviewStars-input");
-
-
     if (token == undefined) {
         wantToSignUp.attr({
             'data-container': 'body',
@@ -280,7 +271,6 @@ function sendComment() {
             'data-content': 'You should sign up'
         });
         wantToSignUp.popover('show');
-
         wantToSignIn.attr({
             'data-container': 'body',
             'data-toggle': 'popover',
@@ -290,29 +280,24 @@ function sendComment() {
         setTimeout(function () {
             wantToSignIn.popover('show');
         }, 1000);
-
         setTimeout(function () {
             wantToSignUp.popover('hide');
             wantToSignIn.popover('hide');
         }, 2000);
     }
-
-
     if (token !== undefined) {
-
-        if (text.value == "" || starsValue == undefined) {
-            if (text.value == "") {
-                textArea.attr({
+        if (text[0].value == "" || starsValue == undefined) {
+            if (text[0].value == "") {
+                text.attr({
                     'data-toggle': 'popover',
                     'data-placement': 'bottom',
                     'data-content': 'Write something here'
                 });
-                textArea.popover("show");
+                text.popover("show");
                 setTimeout(function () {
-                    textArea.popover("hide");
+                    text.popover("hide");
                 }, 3000);
             }
-
             if (starsValue == undefined) {
                 stars.attr({
                     'data-toggle': 'popover',
@@ -323,34 +308,33 @@ function sendComment() {
                 setTimeout(function () {
                     stars.popover("hide");
                 }, 3000);
-
             }
-
-
-        }
-
-        if (text.value !== "" && starsValue !== undefined) {
-            var b = document.getElementById("reviews");
-            var newElement = document.createElement("div");
-            var d = new Date();
-            d = d.toUTCString();
-            newElement.innerHTML = '<div class="list-group-item">' +
-                '<p>' + username.value + ' at ' + d + '</p>' +
-                '<p> Rate: ' + starsValue + '</p>' +
-                '<p>' + text.value + '</p>' + '</div>';
-            b.insertBefore(newElement, b.children[0]);
         }
     }
-
-    if (text.value !== "" && starsValue !== undefined) {
+    if (text[0].value !== "" && starsValue !== undefined) {
         xhr2.open('POST', 'http://smktesting.herokuapp.com/api/reviews/' + val + '');
-            xhr2.setRequestHeader('Content-Type', 'application/json');
-            xhr2.setRequestHeader('Authorization', 'Token ' + token + '');
-            xhr2.send(jsn);
+        xhr2.setRequestHeader('Content-Type', 'application/json');
+        xhr2.setRequestHeader('Authorization', 'Token ' + token + '');
+        xhr2.send(jsn);
+        xhr2.onreadystatechange = function () {
+            if (xhr2.readyState == 4) {
+                if (xhr2.status = 200) {
+                    var b = document.getElementById("reviews");
+                    var newElement = document.createElement("div");
+                    var d = new Date();
+                    d = d.toUTCString();
+                    newElement.innerHTML = '<div class="list-group-item">' +
+                        '<p>' + username[0].value + ' at ' + d + '</p>' +
+                        '<p> Rate: ' + starsValue + '</p>' +
+                        '<p>' + text[0].value + '</p>' + '</div>';
+                    b.insertBefore(newElement, b.children[0]);
 
+                }
+            }
+        }
     }
 }
-//background
+
 
 
 
